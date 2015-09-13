902a22f769ba3bb65732133363ec46d68b55d670
laksamanapaan <laksamanapaan@gmail.com>
2015-09-13 22:48:14 +0800

    second try

--- dna change to protein.cpp
+++ dna change to protein.cpp
@@ -2,6 +2,37 @@
 #include <fstream>
 #include <string>
 using namespace std;
+
+char change(string codon){
+	char symbol;
+	if (codon=="ATT" || codon=="ATC" || codon=="ATA")
+	{
+		symbol='I';
+	}
+	else if (codon=="CTT" || codon=="CTC" || codon=="CTA" || codon=="CTG" )
+	{
+		symbol='K';
+	}
+	else{
+		symbol='L';
+	}
+	return symbol;
+}
+
+string aminoAcid (char symbol){
+	string amino;
+	if (symbol=='I'){
+		amino="isoleucine";
+	}
+	else if (symbol=='K'){
+		amino="leucine";
+	}
+	else {
+		amino="potassium";
+	}
+	return amino;
+}
+
 /*
 void symbol(char dna[codon][nucleotide])
 {
@@ -15,8 +46,8 @@ ofstream outputfile;
 //int codon=10;
 int nucleotide=21;
 //char dna[codon][nucleotide];
-char dna[nucleotide];
-string codon;
+char dna[nucleotide],symbol;
+string codon, amino;
 
 cout<<"your dna sequence is: ";
 inputfile.open("data.txt");
@@ -25,12 +56,25 @@ for (int i=0; i<21; i++)
 	 inputfile>>dna[i];
 	 cout<<dna[i];
 }
+cout<<endl;
 for (int i=0; i<3; i++)
 {
 	 inputfile>>dna[i];
-
 } 
-	 string str(dna);
+	string str(dna);
+	 symbol=change(str);
+	 amino=aminoAcid(symbol);
+	 cout<<symbol<<" - "<<amino;
+/*
+for (int i=3; i<6; i++)
+{
+	 inputfile>>dna[i];
+}  
+ string str(dna);
+	 symbol=change(str);
+	 amino=aminoAcid(symbol);
+	 cout<<symbol<<" - "<<amino;*/
+		 /*
  if (str=="aaa")
 	 {
 	 	codon="Q";
@@ -52,7 +96,7 @@ for (int i=3; i<6; i++)
 	 }
 cout<<codon;
 } 
-
+*/
 /*
 cout<<"your dna sequence is: ";
 for (int i=0; i<10;i++)
